85fedf6e8ce5ac8b560a3c39cbc0b4c248cfab56
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-11-23 20:52:59 -0800

    Book ticket page updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Nav from './components/Nav';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/layout/Home';
+import Bookticket from './components/layout/Bookticket';
+import Movies from './components/layout/Movies';
+
 
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav/>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/book-ticket' exact component={Bookticket} />
+          <Route path='/movies' exact component={Movies} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../images/nav.png'
-import '../styles/nav.css'
+import '../styles/nav.css';
+import { NavLink } from 'react-router-dom'
 
 function Nav() {
     return (
@@ -8,9 +9,15 @@ function Nav() {
             <img src={logo} alt='logo' className='nav-logo'/>
             <div className='nav-container'>
                 <ul className='nav-list'>
-                    <li className='nav-item'>Home</li>
-                    <li className='nav-item'>Book ticket</li>
+                    <NavLink to='/' exact activeClassName='nav-active' className='main-nav'>
+                        <li className='nav-item'>Home</li>
+                    </NavLink>
+                    <NavLink to='/book-ticket' activeClassName='nav-active' className='main-nav'>
+                        <li className='nav-item'>Book ticket</li>
+                    </NavLink>
+                    <NavLink to='/movies' activeClassName='nav-active' className='main-nav'>
                     <li className='nav-item'>movies</li>
+                    </NavLink>
                     <li className='nav-item nav-item-btn'>
                         <button className='nav-btn btn'>Quick booking</button>
                     </li>
