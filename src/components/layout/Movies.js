49b0b259d9cd95a96fa9fa5e0c4b3a3a0d4a13a6
rajkumar <rajkumarpandiyaraj@gmail.com>
2020-11-24 19:52:39 -0800

    ssc clone

--- src/components/layout/Movies.js
+++ src/components/layout/Movies.js
@@ -1,9 +1,139 @@
-import React from 'react'
+import React from 'react';
+import Carousel from 'react-elastic-carousel';
+import m1 from '../../images/m1.jpg';
+import m2 from '../../images/m2.jpg';
+import m3 from '../../images/m3.jpg';
+import m4 from '../../images/m4.jpg';
+import m5 from '../../images/m5.jpg';
+import m6 from '../../images/m6.jpg';
+import m7 from '../../images/m7.jpg';
+import ad1 from '../../images/ad-1.jpg';
+import ad2 from '../../images/ad-2.jpg';
+import ad3 from '../../images/ad-3.jpg';
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
+import {IoIosArrowForward, IoIosArrowDown} from 'react-icons/io';
+import '../../styles/movies.css';
+
 
 function Movies() {
+    const Arrow = ({ type, onClick, isEdge }) => {
+        const pointer = type === "PREV" ? <BiChevronLeft size='2rem'/> : <BiChevronRight size='2rem' />
+
+        return (
+            <button onClick={onClick} disabled={isEdge} className='btn caro-btn'>
+                {pointer}
+            </button>
+        )
+    }
+    
+
     return (
-        <div>
-            <h1>movies</h1>
+        <div className='movies-contain'>
+            <div className='movie-carousel'>
+            <Carousel className='ca' itemsToShow={4} renderArrow={Arrow} >
+                <img src={m1} alt='carousel-img' className='caro' pagination='false'/>
+                <img src={m3} alt='carousel-img' className='caro' pagination='false'/>
+                <img src={m4} alt='carousel-img' className='caro' pagination='false'/>
+                <img src={m5} alt='carousel-img' className='caro' pagination='false'/>
+                <img src={m6} alt='carousel-img' className='caro' pagination='false'/>
+                <img src={m7} alt='carousel-img' className='caro' pagination='false'/>
+                <img src={m2} alt='carousel-img' className='caro' pagination='false'/>
+            </Carousel>
+            </div>
+            <div className='book-container'>
+            <ul className='date-container'>
+                <li className='date-label date-item'>Date</li>
+                <li className='date-arrow date-item'>
+                    <IoIosArrowForward/>
+                </li>
+                <li className='date date-item'>
+                    <span className='day'>mon</span>
+                    <span className=''>23</span>
+                </li>
+                <li className='date date-item'>
+                    <span className='day'>tue</span>
+                    <span className=''>24</span>
+                </li>
+                <li className='date date-item'>
+                    <span className='day'>wed</span>
+                    <span className=''>25</span>
+                </li>
+            </ul>
+            <div className='movie-show-container'>
+                <div className='movie-show-label'>
+                    <p className='movie-label'>Movies</p>
+                    <div className='show-time-container'>
+                        <div className='show-time-label'>
+                            <p>Showtime</p>
+                            <IoIosArrowDown/>
+                        </div>
+                        <div className='availability-label'>
+                            <div className='available'>
+                                <div className='green'></div>
+                                <span>Available</span>
+                            </div>
+                            <div className='sold'>
+                                <div className='red'></div>
+                                <span>Sold out</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='shows'>
+                <div className='show'>
+                    <div className='movie'>
+                        <div className='movie-name'>
+                            <p>biskoth</p>
+                            <div className='lang-dur'>
+                                <span className='lang'>Tamil</span>
+                                <span className='divider'>|</span>
+                                <span className='dur'>106 minute</span>
+                            </div>
+                        </div>
+                        <div className='certification'>
+                            <span>N/A</span>
+                        </div>
+                        <div className='movie-times'>
+                            <span className='movie-time'>10.30pm</span>
+                            <span className='movie-time'>10.30pm</span>
+                            <span className='movie-time'>10.30pm</span>
+                            <span className='movie-time'>10.30pm</span>
+                            <div className='movie-time-divider'></div>
+                            <span className='movie-time'>10.30pm</span>
+                            <span className='movie-time'>10.30pm</span>
+                            <span className='movie-time'>10.30pm</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='show-divider'></div>
+            </div>
+            <div className='book-showcase-container'>
+                <img src={ad1} className='book-showcase' alt='showcase-img'/>
+                <img src={ad2} className='book-showcase' alt='showcase-img'/>
+                <img src={ad3} className='book-showcase' alt='showcase-img'/>
+            </div>
+            <footer className='book-footer'>
+                <div className='box'>
+                    <p>About us</p>
+                    <p>Feedback</p>
+                </div>
+                <div className='box'>
+                    <p>Gallery</p>
+                    <p>careers</p>
+                </div>
+                <div className='box'>
+                <p>Privacy</p>
+                    <p>Legal</p>
+                </div>
+                <div className='box'>
+                    <p>Powered by: BookMyShow.com</p>
+                </div>
+                <div className='box'>
+                    <p>2020 srisakthi.net - All rights reserved</p>
+                </div>
+            </footer>
+        </div>
         </div>
     )
 }
